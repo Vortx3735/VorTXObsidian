5ccf988ff2ab8c6e671ac53fb4a3cf2c53df49c1
Chirag Tripathi <87745910+chxrro@users.noreply.github.com>
2024-05-21 21:14:44 -0500

    FIx Logo Issues

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,12 +1,18 @@
+import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
+import { joinSegments } from "../util/path"
+import { i18n } from "../i18n"
 
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
+  const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
+  const baseDir = pathToRoot(fileData.slug!)
+  const iconPath = joinSegments(baseDir, "static/vortxlogo.png")
   return (
     <h1 class={classNames(displayClass, "page-title")}>
-      <a>
-        <img src="../static/vortxlogo.png"/>
+      <a href={baseDir}>
+        <img class="Logo" src={iconPath} alt={title}/>
       </a>
     </h1>
   )
